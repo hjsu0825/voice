45b9c15689dc2c430b29b5f20e5d1238ab170fb4
hjsu0825 <hjsu0825@naver.com>
2024-04-16 02:06:12 +0900

    1

--- static/script.js
+++ static/script.js
@@ -1,7 +1,40 @@
+// let audioContext;
+// let pitchShifter;
+// let micStream;
+
+// window.onload = () => {
+//     audioContext = new AudioContext();
+//     setupMicrophone();
+// };
+
+// async function setupMicrophone() {
+//     try {
+//         const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+//         micStream = audioContext.createMediaStreamSource(stream);
+//         setupPitchShifter();
+//     } catch (error) {
+//         console.error('Error accessing the microphone', error);
+//     }
+// }
+
+// function setupPitchShifter() {
+//     pitchShifter = audioContext.createGain(); // Default gain node to start with
+//     micStream.connect(pitchShifter);
+//     pitchShifter.connect(audioContext.destination);
+// }
+
+// function changePitch(type) {
+//     const pitchRatio = type === 'high' ? 1.5 : type === 'low' ? 0.5 : 1.0;
+//     if (pitchShifter) {
+//         pitchShifter.gain.value = pitchRatio;
+//     }
+// }
+
 // static/script.js
 let audioContext;
-let pitchShifter;
 let micStream;
+let pitchProcessor;
+let pitchNode;
 
 window.onload = () => {
     audioContext = new AudioContext();
@@ -12,21 +45,24 @@ async function setupMicrophone() {
     try {
         const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
         micStream = audioContext.createMediaStreamSource(stream);
-        setupPitchShifter();
+        setupPitchProcessor();
     } catch (error) {
         console.error('Error accessing the microphone', error);
     }
 }
 
-function setupPitchShifter() {
-    pitchShifter = audioContext.createGain(); // Default gain node to start with
-    micStream.connect(pitchShifter);
-    pitchShifter.connect(audioContext.destination);
+function setupPitchProcessor() {
+    pitchNode = new PitchProcessor({audioContext: audioContext});
+    pitchProcessor = audioContext.createScriptProcessor(4096, 1, 1);
+    micStream.connect(pitchProcessor);
+    pitchProcessor.connect(pitchNode.input);
+    pitchNode.output.connect(audioContext.destination);
+    pitchNode.onaudioprocess = function(data) {
+        pitchNode.transpose = pitchNode.transpose; // You can modify this value based on buttons
+    };
 }
 
 function changePitch(type) {
-    const pitchRatio = type === 'high' ? 1.5 : type === 'low' ? 0.5 : 1.0;
-    if (pitchShifter) {
-        pitchShifter.gain.value = pitchRatio;
-    }
+    const semitones = type === 'high' ? 12 : type === 'low' ? -12 : 0;
+    pitchNode.transpose = semitones;
 }
