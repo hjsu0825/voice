f7fdd6a7d0600146ccea8f838f345146c9b468e0
hjsu0825 <hjsu0825@naver.com>
2024-04-16 02:27:28 +0900

    1

--- static/script.js
+++ static/script.js
@@ -1,38 +1,8 @@
-// let audioContext;
-// let pitchShifter;
-// let micStream;
-
-// window.onload = () => {
-//     audioContext = new AudioContext();
-//     setupMicrophone();
-// };
-
-// async function setupMicrophone() {
-//     try {
-//         const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-//         micStream = audioContext.createMediaStreamSource(stream);
-//         setupPitchShifter();
-//     } catch (error) {
-//         console.error('Error accessing the microphone', error);
-//     }
-// }
-
-// function setupPitchShifter() {
-//     pitchShifter = audioContext.createGain(); // Default gain node to start with
-//     micStream.connect(pitchShifter);
-//     pitchShifter.connect(audioContext.destination);
-// }
-
-// function changePitch(type) {
-//     const pitchRatio = type === 'high' ? 1.5 : type === 'low' ? 0.5 : 1.0;
-//     if (pitchShifter) {
-//         pitchShifter.gain.value = pitchRatio;
-//     }
-// }
-
+// static/script.js
 let audioContext;
 let micStream;
 let jungle;
+let effectActive = false;
 
 window.onload = () => {
     audioContext = new AudioContext();
@@ -53,18 +23,26 @@ function setupJungle() {
     jungle = new Jungle(audioContext);
     micStream.connect(jungle.input);
     jungle.output.connect(audioContext.destination);
+    effectActive = true;
 }
 
 function changePitch(type) {
-    switch (type) {
-        case 'high':
-            jungle.setPitchOffset(2.0); // 이 값을 조정하여 피치를 높입니다.
-            break;
-        case 'low':
-            jungle.setPitchOffset(0.5); // 이 값을 조정하여 피치를 낮춥니다.
-            break;
-        case 'normal':
-            jungle.setPitchOffset(1.0); // 원래대로 복원
-            break;
+    if (type === 'off') {
+        if (effectActive) {
+            jungle.output.disconnect();
+            micStream.disconnect();
+            micStream.connect(audioContext.destination);
+            effectActive = false;
+        }
+    } else {
+        if (!effectActive) {
+            micStream.disconnect();
+            micStream.connect(jungle.input);
+            jungle.output.connect(audioContext.destination);
+            effectActive = true;
+        }
+
+        const pitchRatio = type === 'high' ? 2.0 : type === 'low' ? 0.5 : 1.0;
+        jungle.setPitchOffset(pitchRatio);
     }
 }
