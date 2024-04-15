d7bdb0bcd26f2276f7f7380d7595659ead5fda5c
hjsu0825 <hjsu0825@naver.com>
2024-04-16 02:11:59 +0900

    1

--- static/script.js
+++ static/script.js
@@ -31,10 +31,10 @@
 // }
 
 // static/script.js
+// static/script.js
 let audioContext;
 let micStream;
-let pitchProcessor;
-let pitchNode;
+let jungle;
 
 window.onload = () => {
     audioContext = new AudioContext();
@@ -45,24 +45,28 @@ async function setupMicrophone() {
     try {
         const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
         micStream = audioContext.createMediaStreamSource(stream);
-        setupPitchProcessor();
+        setupJungle();
     } catch (error) {
         console.error('Error accessing the microphone', error);
     }
 }
 
-function setupPitchProcessor() {
-    pitchNode = new PitchProcessor({audioContext: audioContext});
-    pitchProcessor = audioContext.createScriptProcessor(4096, 1, 1);
-    micStream.connect(pitchProcessor);
-    pitchProcessor.connect(pitchNode.input);
-    pitchNode.output.connect(audioContext.destination);
-    pitchNode.onaudioprocess = function(data) {
-        pitchNode.transpose = pitchNode.transpose; // You can modify this value based on buttons
-    };
+function setupJungle() {
+    jungle = new Jungle(audioContext);
+    micStream.connect(jungle.input);
+    jungle.output.connect(audioContext.destination);
 }
 
 function changePitch(type) {
-    const semitones = type === 'high' ? 12 : type === 'low' ? -12 : 0;
-    pitchNode.transpose = semitones;
+    switch (type) {
+        case 'high':
+            jungle.setPitchOffset(2.0); // 이 값을 조정하여 피치를 높입니다.
+            break;
+        case 'low':
+            jungle.setPitchOffset(0.5); // 이 값을 조정하여 피치를 낮춥니다.
+            break;
+        case 'normal':
+            jungle.setPitchOffset(1.0); // 원래대로 복원
+            break;
+    }
 }
