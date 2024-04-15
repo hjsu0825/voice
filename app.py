14675ded1d4f7b7d27d97053b4109e0df8f76d67
hjsu0825 <hjsu0825@naver.com>
2024-04-16 02:02:06 +0900

    1

--- app.py
+++ app.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
