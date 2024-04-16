cbbf84a9ac959fb093a953e776df597429c1e4f6
hjsu0825 <hjsu0825@naver.com>
2024-04-16 15:17:26 +0900

    1

--- static/script.js
+++ static/script.js
@@ -14,7 +14,7 @@ async function setupMicrophone() {
         micStream = audioContext.createMediaStreamSource(stream);
         setupJungle();
     } catch (error) {
-        console.error('Error accessing the microphone', error);
+        console.error('마이크 오류 발생', error);
     }
 }
 
@@ -41,7 +41,7 @@ function changePitch(type) {
             effectActive = true;
         }
 
-        const pitchRatio = type === 'high' ? 1.5 : 0.5; // 'high' 또는 'low'에 해당하는 피치 비율을 설정
+        const pitchRatio = type === 'high' ? 1.5 : 0.5;
         jungle.setPitchOffset(pitchRatio);
     }
 }
